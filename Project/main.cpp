8db689a8309a3293055dc0d2b2601e807c7c29fc
Ww1asS <alex1602serov@gmail.com>
2025-09-14 19:25:06 +0300

    1

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,44 +1,14 @@
-#include "raylib.h"
+#include <raylib.h>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+int main() {
+    SetConfigFlags(FLAG_WINDOW_HIGHDPI); // Поддержка Retina
+    InitWindow(800, 450, "Raylib Test");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        ClearBackground(RAYWHITE);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
